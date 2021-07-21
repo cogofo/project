cf352fce37927d7454ed9fe4adc2e89d8dbd8a20
Charles <cogodo14@gmail.com>
2021-07-21 10:38:19 -0500

    Initial Commit

---
+++ Gify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gify
+//
+//  Created by Charles Ogodo on 7/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
